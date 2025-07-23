58dd61f61d61c6ef9c0a72b01e5e991ca1d5b357
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-07-23 19:44:21 +0400

    week-2 lesson

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+import { Test } from './Components/test/test';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Test],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('week2');
