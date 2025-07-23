dcfd63e03ac2f7424d8ec17fa3c874f2abf91b78
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-07-23 20:05:03 +0400

    Second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
 import { Test } from './Components/test/test';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer, Test],
+  imports: [Navbar, Footer, Test],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
