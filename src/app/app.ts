8eb443a9a4d4f349595af76537d53dbc039d6318
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-07-25 18:37:53 +0400

    add filter section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewEncapsulation } from '@angular/core';
 import { Navbar } from './Components/navbar/navbar';
+import { Filter } from './Components/filter/filter';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Filter],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class App {
   protected readonly title = signal('week2');
